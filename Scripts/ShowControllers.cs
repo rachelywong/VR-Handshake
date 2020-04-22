e4075b34b6dffb1a4e5fb55895c183bd705f2d37
Rachel Wong <rachel.wong@hotmail.ca>
2020-04-22 11:38:35 -0600

    Add files via upload

---
+++ Scripts/ShowControllers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+// If we want to show controllers or not
+public class ShowControllers : MonoBehaviour
+{
+    public bool showController = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var hand in Player.instance.hands)
+        {
+            if(showController)
+            {
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController); // hold the controller properly
+            }
+            else
+            {
+                hand.HideController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithoutController);
+
+            }
+        }
+
+    }
+}
---
+++ Scripts/SimpleAttach.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour
+{
+    private Interactable interactable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint(); // Show controller hint
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint(); // Hide controller hint
+    }
+
+    private void OnHandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        // Grab the object
+        if (interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+        }
+
+        // Release the object
+        else if (isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+}
